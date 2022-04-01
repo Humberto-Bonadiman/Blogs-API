70132adb8dbe2357133ddbdd94fa0e64a4addff9
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-04-01 17:39:08 -0400

    remove comments

--- middlewares/authCategories.js
+++ middlewares/authCategories.js
@@ -1,25 +1,3 @@
-/* require('dotenv').config();
-const jwt = require('jsonwebtoken');
-
-const { Categories } = require('../models');
-
-const validateCategory = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-  
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const category = await Categories.findByPk(decoded.data.id);
-
-    if (!category) {
-      return res.status(401).json({ message: 'Expired or invalid token' });
-    }
-
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Expired or invalid token' });
-  }
-}; */
-
 const validateName = (req, res, next) => {
   const { name } = req.body;
   if (!name) {
@@ -30,6 +8,5 @@ const validateName = (req, res, next) => {
 };
 
 module.exports = {
-  // validateCategory,
   validateName,
 };
