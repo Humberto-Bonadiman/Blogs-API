c8ea6231a51fd9764c49991f272cd002210c9ea5
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-04-03 17:45:47 -0400

    create test for POST '/user'

---
+++ tests/mock/userMock.js
@@ -0,0 +1,13 @@
+const userMock = {
+  id: 3,
+  displayName: 'Fernando Alonso',
+  email: 'alonso@gmail.com',
+  password: '123456',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Alonso_2016.jpg/479px-Alonso_2016.jpg'
+};
+
+const tokenUser = {
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo0LCJkaXNwbGF5TmFtZSI6IkZlcm5hbmRvIEFsb25zbyIsImVtYWlsIjoiYWxvbnNvQGdtYWlsLmNvbSIsImltYWdlIjoiaHR0cHM6Ly91cGxvYWQud2lraW1lZGlhLm9yZy93aWtpcGVkaWEvY29tbW9ucy90aHVtYi8yLzJiL0Fsb25zb18yMDE2LmpwZy80NzlweC1BbG9uc29fMjAxNi5qcGcifSwiaWF0IjoxNjQ5MDE5NTIwLCJleHAiOjE2NDk2MjQzMjB9.PNl4_lxsC45Ce6YsTnL5bXSdka4ft9jH6EFvY0dAAvM'
+}
+
+module.exports = { userMock, tokenUser };
