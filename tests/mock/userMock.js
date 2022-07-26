802867d2c7ed2eb0e4ad7ccdcd0a83b82480eb33
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-07-26 16:21:01 -0400

    feat: add tests for POST '/login' and GET '/user'

--- tests/mock/userMock.js
+++ tests/mock/userMock.js
@@ -10,4 +10,26 @@ const tokenUser = {
   token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo0LCJkaXNwbGF5TmFtZSI6IkZlcm5hbmRvIEFsb25zbyIsImVtYWlsIjoiYWxvbnNvQGdtYWlsLmNvbSIsImltYWdlIjoiaHR0cHM6Ly91cGxvYWQud2lraW1lZGlhLm9yZy93aWtpcGVkaWEvY29tbW9ucy90aHVtYi8yLzJiL0Fsb25zb18yMDE2LmpwZy80NzlweC1BbG9uc29fMjAxNi5qcGcifSwiaWF0IjoxNjQ5MDE5NTIwLCJleHAiOjE2NDk2MjQzMjB9.PNl4_lxsC45Ce6YsTnL5bXSdka4ft9jH6EFvY0dAAvM'
 }
 
-module.exports = { userMock, tokenUser };
+const loginMock = {
+  email: 'lewishamilton@gmail.com',
+  password: '123456',
+};
+
+const getAllUsers = [
+  {
+    id: 1,
+    displayName: 'Lewis Hamilton',
+    email: 'lewishamilton@gmail.com',
+    password: '123456',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
+  },
+  {
+    id: 2,
+    displayName: 'Michael Schumacher',
+    email: 'MichaelSchumacher@gmail.com',
+    password: '123456',
+    image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
+  },
+]
+
+module.exports = { userMock, tokenUser, loginMock, getAllUsers };
--- tests/user.test.js
+++ tests/user.test.js
@@ -4,7 +4,7 @@ const chaiHttp = require('chai-http');
 
 const app = require('../api/app');
 
-const { userMock } = require('./mock/userMock');
+const { userMock, loginMock, getAllUsers } = require('./mock/userMock');
 const { Users } = require('../models');
 
 chai.use(chaiHttp);
@@ -30,7 +30,7 @@ describe('Authentication test in POST "/user"', () => {
   });
 
   describe('Should return the expected data', () => {
-     it('should return the status code 200', async() => {
+     it('should return the status code 201', async() => {
       chaiHttpResponse = await chai.request(app).post('/user').send(userPost);
       expect(chaiHttpResponse).to.have.status(201);
     });
@@ -133,6 +133,128 @@ describe('Authentication test in POST "/user"', () => {
 
       expect(chaiHttpResponse).to.have.status(409);
       expect(chaiHttpResponse.body.message).to.be.equal('User already registered');
+    });
+  });
+});
+
+describe('Authentication test in POST "/login"', () => {
+  let chaiHttpResponse;
+  const userLogin = {
+    email: 'lewishamilton@gmail.com',
+    password: '123456',
+  };
+  let errorLogin = {};
+
+  before(async () => {
+    sinon.stub(Users, 'create').resolves(loginMock);
+  });
+
+  after(() => {
+    Users.create.restore();
+  });
+
+  describe('Should return the expected data', () => {
+    it('should return the status code 200', async() => {
+     chaiHttpResponse = await chai.request(app).post('/login').send(userLogin);
+     expect(chaiHttpResponse).to.have.status(200);
+    });
+
+  it('should return the key token', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login').send(userLogin);
+     expect(chaiHttpResponse.body).to.have.property('token');
+    });
+  });
+
+  it('It will be validated that the "email" field is mandatory', async () => {
+    errorLogin = {
+      password: '123456'
+    };
+    chaiHttpResponse = await chai.request(app).post('/login').send(errorLogin);
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body.message).to.be.equal('"email" is required');
+  });
+
+  it('It will be validated that the "password" field is mandatory', async () => {
+    errorLogin = {
+      email: 'lewishamilton@gmail.com'
+    };
+    chaiHttpResponse = await chai.request(app).post('/login').send(errorLogin);
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body.message).to.be.equal('"password" is required');
+  });
+
+  it('It will be that the "email" is not allowed to be empty', async () => {
+    errorLogin = {
+      email: '',
+      password: '123456',
+    };
+    chaiHttpResponse = await chai.request(app).post('/login').send(errorLogin);
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body.message).to.be.equal('"email" is not allowed to be empty');
+  });
+
+  it('It will be that the "password" is not allowed to be empty', async () => {
+    errorLogin = {
+      email: 'lewishamilton@gmail.com',
+      password: '',
+    };
+    chaiHttpResponse = await chai.request(app).post('/login').send(errorLogin);
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body.message).to.be.equal('"password" is not allowed to be empty');
+  });
+
+  it(`It will be validated that it is not possible to login with a
+  user that does not exist`, async () => {
+    errorLogin = {
+      email: 'aasd@gmail.com',
+      password: '123456',
+    };
+    chaiHttpResponse = await chai.request(app).post('/login').send(errorLogin);
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body.message).to.be.equal('Invalid fields');
+  });
+});
+
+describe('Authentication test in GET "/user"', () => {
+  let chaiHttpResponse;
+  let token;
+  let errorLogin = {};
+
+  before(async () => {
+    sinon.stub(Users, 'create').resolves(loginMock);
+  });
+
+  after(() => {
+    Users.create.restore();
+  });
+
+  describe('Should return the expected data', () => {
+    it('should return the status code 200', async () => {
+      token = await chai.request(app).post('/login').send(loginMock);
+      chaiHttpResponse = await chai.request(app).get('/user').set('authorization', token.body.token);
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+
+    it('should return a list of all users', async () => {
+      token = await chai.request(app).post('/login').send(loginMock);
+      chaiHttpResponse = await chai.request(app).get('/user').set('authorization', token.body.token);
+      const firstUser = chaiHttpResponse.body[0];
+      expect(chaiHttpResponse.body[0]).to.include({
+        id: 1,
+        displayName: "Lewis Hamilton",
+        email: "lewishamilton@gmail.com",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg"
+      });
+      expect(firstUser.displayName).to.equal('Lewis Hamilton');
+      expect(firstUser.email).to.equal('lewishamilton@gmail.com');
+      expect(firstUser.image).to.equal(
+        'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg'
+      );
     });
   });
 });
